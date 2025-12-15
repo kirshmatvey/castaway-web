03bdc5b85fb1c08b855f7fef77d7da4174b0b5b1
kirshmatvey <matveykirshchin01@gmail.com>
2025-12-15 17:30:48 +0400

    burger menu is half done

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const burgerMenu = document.querySelector('.burger-menu-icon');
 const burgerMenuCloser = document.querySelector('.burger-menu-closer');
 
+const model = {
+    list: [
+        {id: Date.now(), content: 'Home', link: '#home'},
+        {id: Date.now(), content: 'Episodes', link: '#episodes'},
+        {id: Date.now(), content: 'About', link: '#about'},
+        {id: Date.now(), content: 'Contact', link: '#contact'},
+    ]
+}
+
 burgerMenu.addEventListener('click', (event) => {
-    // const blur  = document.getElementById('cover');
     const header = document.querySelector('.header');
 
     const blur = document.createElement('div')
+    const listContainer = document.createElement('div');
+    listContainer.classList.add('list-container');
+    const list = createList()
+
+    listContainer.append(list)
+
+
 
     blur.setAttribute('id', 'cover');
     blur.classList.add('page-cover');
@@ -18,12 +33,15 @@ burgerMenu.addEventListener('click', (event) => {
     burgerMenuCloser.classList.remove('burger-menu-opened');
 
     document.body.insertBefore(blur, document.body.lastChild);
+    document.body.insertBefore(listContainer, document.body.lastChild);
 })
 
 burgerMenuCloser.addEventListener('click', (event) => {
     const blur  = document.getElementById('cover');
     const header = document.querySelector('.header');
+    const burgerList = document.querySelector('.list-container');
 
+    burgerList.remove()
     blur.remove()
     header.classList.remove('header-burger-menu');
 
@@ -35,3 +53,37 @@ burgerMenuCloser.addEventListener('click', (event) => {
     burgerMenuCloser.classList.remove('burger-menu-closed');
 })
 
+function createList() {
+    const list = document.createElement('ul');
+    list.classList.add('burger-menu-list');
+
+    model.list.forEach((item) => {
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+
+        link.textContent = item.content;
+        link.setAttribute('href', item.link);
+
+        listItem.append(link);
+        list.append(listItem);
+    })
+
+    list.addEventListener('click', (event) => {
+        const blur  = document.getElementById('cover');
+        const header = document.querySelector('.header');
+        const burgerList = document.querySelector('.list-container');
+
+        burgerList.remove()
+        blur.remove()
+        header.classList.remove('header-burger-menu');
+
+        // потом создать MVC структуру и вынести эту функцию
+        burgerMenu.classList.add('burger-menu-closed');
+        burgerMenu.classList.remove('burger-menu-opened');
+
+        burgerMenuCloser.classList.add('burger-menu-opened');
+        burgerMenuCloser.classList.remove('burger-menu-closed');
+    })
+
+    return list;
+}
